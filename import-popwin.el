616accb0f7e620b07aeb0e7313677b2d6d29d403
Syohei YOSHIDA <syohex@gmail.com>
2014-03-13 16:43:51 +0900

    Update 'cl-lib' version

--- import-popwin.el
+++ import-popwin.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-import-popwin
 ;; Version: 0.06
-;; Package-Requires: ((popwin "0.6") (cl-lib "0.3"))
+;; Package-Requires: ((popwin "0.6") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
