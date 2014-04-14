3f1df467191123fbdefddc3e65ceb506dd3709c2
Syohei YOSHIDA <syohex@gmail.com>
2014-04-14 14:41:16 +0900

    Bump up version and update Changes

--- import-popwin.el
+++ import-popwin.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-import-popwin
-;; Version: 0.07
+;; Version: 0.08
 ;; Package-Requires: ((popwin "0.6") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
