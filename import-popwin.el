bb05a9e226f8c63fe7b18a3e92010357049ab5ba
Syohei YOSHIDA <syohex@gmail.com>
2017-02-18 23:07:23 +0900

    Add package commentary

--- import-popwin.el
+++ import-popwin.el
@@ -1,6 +1,6 @@
 ;;; import-popwin.el --- popwin buffer near by import statements with popwin -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016 by Syohei YOSHIDA
+;; Copyright (C) 2017 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-import-popwin
@@ -22,7 +22,11 @@
 
 ;;; Commentary:
 
-;; Inspired by http://shibayu36.hatenablog.com/entry/2013/01/03/223056
+;; Pop up buffer with pop-win near by import statements. For example
+;; in C language, pop up near by include statements.
+;;
+;; This package is inspired by this article
+;;  - http://shibayu36.hatenablog.com/entry/2013/01/03/223056
 
 ;;; Code:
 
